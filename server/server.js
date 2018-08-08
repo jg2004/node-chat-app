01a0ebcd370d06490d93e1ed5221c2b22aaeda74
jg2004 <jgdeveloper2004@gmail.com>
2018-08-07 22:44:27 -0400

    initial commit

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const express = require('express');
+
+
+const app = express();
+const publicPath = path.join(__dirname, '..', '/public')
+app.use(express.static(publicPath))
+
+const port = process.env.PORT || 3000
+
+
+
+app.listen(port, () => {
+  console.log(`ListeServer is up on port ${port}`)
+})
