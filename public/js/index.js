3a45d207eb9e87bd3033867369a15902b769cb98
jg2004 <jgdeveloper2004@gmail.com>
2018-08-09 22:54:05 -0400

    added form to submit message and added html to display messages

--- public/js/index.js
+++ public/js/index.js
@@ -1,15 +1,34 @@
 const socket = io();
 socket.on('connect', function () {
   console.log('connected to server')
-  })
-
-socket.on('newMessage', (message) => {
-  console.log('newMessage',message)
 })
 
-socket.on('newUserMessage',(message)=>{
+socket.on('newMessage', (message) => {
+  console.log('newMessage', message)
+
+  const li = document.createElement('li');
+  li.textContent = `${message.from}: ${message.text}`
+  messages.appendChild(li)
+})
+
+socket.on('newUserMessage', (message) => {
   console.log(message)
 })
 socket.on('disconnect', function () {
-  console.log('Disconnected from server');
+  console.log('Disconnected from server');})
+
+
+const form = document.getElementById('message-form');
+const messages = document.getElementById('messages');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  socket.emit('createMessage', {
+    from: 'User',
+    text: document.getElementById('form-input').value
+  }, () => {
+    console.log('got it')
+
+  })
 })
--- server/server.js
+++ server/server.js
@@ -12,21 +12,19 @@ const {
   generateMessage
 } = require('./utils/message')
 
-
 io.on('connection', (socket) => {
   console.log('New user connected')
-  socket.emit('newUserMessage', generateMessage('Admin', 'Welcome to the chat app'));
-  socket.broadcast.emit('newUserMessage', generateMessage('Admin', 'A new user just joined the chat'))
-  socket.on('createMessage', (message) => {
+  socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
+  socket.broadcast.emit('newMessage', generateMessage('Admin', 'A new user just joined the chat'))
+  socket.on('createMessage', (message,callback) => {
     console.log('createMessage', message)
     io.emit('newMessage', generateMessage(message.from, message.text))
+    callback();
   })
   socket.on('disconnect', () => {
     console.log('client disconnected')
   })
 })
-
-
 
 server.listen(port, () => {
   console.log(`Server is up on port ${port}`)
